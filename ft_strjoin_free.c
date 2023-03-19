6571492e7d74d581bcd93030269b2355ac237751
Jisse Meruma <jissemeruma@gmail.com>
2023-03-19 16:30:24 +0100

    fixed a probabel leak

--- ft_strjoin_free.c
+++ ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: jmeruma <jmeruma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 15:05:51 by jmeruma           #+#    #+#             */
-/*   Updated: 2023/01/25 14:45:55 by jmeruma          ###   ########.fr       */
+/*   Updated: 2023/03/19 16:29:29 by jmeruma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strjoin_free(char *s1, char const *s2)
 	total = ft_strlen(s1) + ft_strlen(s2);
 	ptr = (char *)malloc(total + 1);
 	if (!ptr)
-		return (NULL);
+		return (free(s1), NULL);
 	ft_strlcpy(ptr, (char *)s1, (ft_strlen(s1) + 1));
 	ft_strlcat(ptr, (char *)s2, (total + 1));
 	free(s1);
